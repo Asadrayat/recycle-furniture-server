e3cebe7a752be98261ba0bc7fdc9d1e4036f0e56
Asadrayat <asad.rayat4@gmail.com>
2022-11-26 02:40:33 +0600

    added

--- index.js
+++ index.js
@@ -17,21 +17,51 @@ async function run() {
     try {
         const catagoriesCollection = client.db('recycle-furniture').collection('catagories');
         const diningCollection = client.db('recycle-furniture').collection('dining');
+        const bedroomCollection = client.db('recycle-furniture').collection('bedroom');
+        const livingCollection = client.db('recycle-furniture').collection('living');
+        const bookingsCollection = client.db('recycle-furniture').collection('bookings');
         app.get('/catagoryOptions', async (req, res) => {
             const query = {};
             const result = await catagoriesCollection.find(query).toArray();
             res.send(result);
         });
-        app.get('/dining', async (req, res) => {
+        app.get('/Dining', async (req, res) => {
             const query = {};
             const result = await diningCollection.find(query).toArray();
             res.send(result);
         });
+        app.get('/Bedroom', async (req, res) => {
+            const query = {};
+            const result = await bedroomCollection.find(query).toArray();
+            res.send(result);
+        });
+        app.get('/Living', async (req, res) => {
+            const query = {};
+            const result = await livingCollection.find(query).toArray();
+            res.send(result);
+        });
         app.get('/catagoryOptions/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id:ObjectId(id) };
+            const query = { _id: ObjectId(id) };
             const catagories = await catagoriesCollection.findOne(query);
             res.send(catagories);
+        });
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            // const query = {
+            //     // appointmentDate: booking.appointmentDate,
+            //     email: booking.email,
+            // }
+            // treatement: booking.treatement
+            // }
+            // const alreadyBooked = await bookingsCollection.find(query).toArray();
+            // if (alreadyBooked.length) {
+            //     const message = `You have already booking on ${booking.appointmentDate}`;
+            //     return res.send({ acknowledged: false, message })
+
+            // }
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
         });
 
     }
