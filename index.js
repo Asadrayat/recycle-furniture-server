c4fa4e295d3848e5e3b421263838dc886e573610
Asadrayat <asad.rayat4@gmail.com>
2022-11-26 03:44:57 +0600

    jwt started

--- index.js
+++ index.js
@@ -53,6 +53,17 @@ async function run() {
             // console.log(result);
             res.send(result);
         })
+        app.get('/bookings',  async (req, res) => {
+            const email = req.query.email;
+            // const decodedEmail = req.decoded.email;
+
+            // if (email !== decodedEmail) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+            // }
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             // const query = {
